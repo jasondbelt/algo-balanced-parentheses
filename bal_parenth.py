ef8d739b85d12e4caa361ea16ceca80cd5b31bb6
Jason Belt <jasondbelt@gmail.com>
2025-02-10 08:12:45 -0600

    all tests passed

---
+++ bal_parenth.py
@@ -0,0 +1,25 @@
+def balanced_parentheses(str):
+    counter = 0
+
+    for char in range(len(str)):
+        if str[char] == '(' or str[char] == '{' or str[char] == '[':
+            counter += 1
+        elif str[char] == ')' or str[char] == '}' or str[char] == ']':
+            counter -= 1
+        # exit function and return False if counter < 0, meaning unbalanced chars    
+        if counter < 0:
+            return False
+
+    # need to explicitly specify since 0 is a falsy value
+    return True if counter == 0 else False
+
+
+print(balanced_parentheses("()")) # True
+print(balanced_parentheses("())")) # False
+print(balanced_parentheses("(){}")) # True
+print(balanced_parentheses("({})")) # True
+print(balanced_parentheses("(){}[])")) # False
+print(balanced_parentheses("][")) # False
+print(balanced_parentheses("(({}")) # False
+print(balanced_parentheses("}([]){")) # False
+print(balanced_parentheses("{[()]({})}()")) # True
